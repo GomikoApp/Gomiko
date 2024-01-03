d83d47b1dd35254770fa99424fff5ee0e2fc2951
Marouane <57333995+riimuru@users.noreply.github.com>
2024-01-03 17:26:23 -0500

    feat: Add NavigationBar (#5)
    * Add NavigationBar
    
    * feat(NavigationBar): remove signout and add leaderboard
    
    * change MaterialApp tititle to Gomiko

---
+++ lib/pages/profile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => ProfilePageState();
+}
+
+class ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hello, world! I'm ${widget.title}."),
+      ),
+    );
+  }
+}
