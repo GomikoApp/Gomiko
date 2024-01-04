1d5f93db760fbe55a9627d310b0a5631fd945b2d
lausan3 <lauanthony4455@gmail.com>
2024-01-04 18:05:51 -0500

    Added a blank signup page

---
+++ lib/pages/signup.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("This is signup!")
+      )
+    );
+  }
+}
