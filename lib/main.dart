1c0fcc29053f7a54fa40b2c3cc81ab1e9ba178e7
lausan3 <lauanthony4455@gmail.com>
2024-01-04 22:38:42 -0500

    Integrated Provider ChangeNotifier to app to maintain a global app state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/home.dart';
 import 'pages/profile.dart';
@@ -61,54 +62,72 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gomiko',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Colors.blue, secondary: Colors.red),
-      ),
-      darkTheme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Gomiko'),
+    // See MyAppChangeNotifier class below for details on change notifiers.
+    return ChangeNotifierProvider(
+      create: (context) => MyAppChangeNotifier(),
+      child: MaterialApp(
+        title: 'Gomiko',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.blue, secondary: Colors.red),
         ),
-        body: Center(
-          child: _pages.elementAt(_selectedIndex),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: SizedBox(
-          height: 75,
-          width: 75,
-          child: FloatingActionButton(
-            //TODO: Add functionality to scan button
-            onPressed: () {},
-            tooltip: 'Scan',
-            shape: const CircleBorder(),
-            child: const Icon(
-              Icons.camera_alt,
-              size: 40,
-            ),
+        darkTheme: ThemeData.dark(),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Gomiko'),
           ),
-        ),
-        bottomNavigationBar: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            NavigationBar(
-              selectedIndex: _selectedIndex,
-              onDestinationSelected: _onDestinationSelected,
-              destinations: _destinations,
-            ),
-            Positioned(
-              bottom: MediaQuery.of(context).size.height *
-                  0.03, // Adjust this to adjust the position of the text
-              child: const Text(
-                'Scan',
+          body: Center(
+            child: _pages.elementAt(_selectedIndex),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: SizedBox(
+            height: 75,
+            width: 75,
+            child: FloatingActionButton(
+              //TODO: Add functionality to scan button
+              onPressed: () {},
+              tooltip: 'Scan',
+              shape: const CircleBorder(),
+              child: const Icon(
+                Icons.camera_alt,
+                size: 40,
               ),
             ),
-          ],
+          ),
+          bottomNavigationBar: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              NavigationBar(
+                selectedIndex: _selectedIndex,
+                onDestinationSelected: _onDestinationSelected,
+                destinations: _destinations,
+              ),
+              Positioned(
+                bottom: MediaQuery.of(context).size.height *
+                    0.03, // Adjust this to adjust the position of the text
+                child: const Text(
+                  'Scan',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+/// This change notifier maintains a global state for the app.
+/// Reference: https://docs.flutter.dev/data-and-backend/state-mgmt/simple
+/// In this change notifier, put anything that you want to be accessible throughout the whole app.
+/// Then, in your widgets, you can create a variable that watches the root widget's change notifier state using 'BuildContext.watch<MyAppChangeNotifier>()'
+class MyAppChangeNotifier extends ChangeNotifier {
+  // Variable that keeps track of if the user is logged into the app.
+  // This is a temporary solution to imitate how the actual app will work before Firebase authentication.
+  bool loggedIn = false;
+
+  void testLogInToUser() {
+    loggedIn = true;
   }
 }
