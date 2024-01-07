31c2f2793cfc3695537e467387317cf9c829c652
Marouane <57333995+riimuru@users.noreply.github.com>
2024-01-07 00:42:10 -0500

    Add capitalize string extension

---
+++ lib/utils/extensions.dart
@@ -0,0 +1,19 @@
+extension StringExtension on String {
+  /// Returns a copy of this string having its first letter uppercased.
+  ///
+  /// If this string is empty or already starts with an uppercase letter,
+  /// it is returned unchanged.
+  ///
+  /// Example:
+  /// ```dart
+  /// void main() {
+  ///   print('hello'.capitalize()); // 'Hello'
+  ///   print('Hello'.capitalize()); // 'Hello'
+  ///   print(''.capitalize()); // ''
+  /// }
+  /// ```
+  String capitalize() {
+    if (isEmpty) return this;
+    return this[0].toUpperCase() + substring(1);
+  }
+}
