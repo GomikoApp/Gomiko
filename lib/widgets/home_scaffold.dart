adb5b6b922f7d1a08c3dd28fc845be64d69e9a00
Shi Tao Luo <sluo5991@gmail.com>
2024-04-24 00:35:32 -0400

    removed sized box to fix renderflow error in bottom navigation bar

--- lib/widgets/home_scaffold.dart
+++ lib/widgets/home_scaffold.dart
@@ -44,46 +44,44 @@ class _MyHomeScaffoldState extends State<HomeScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Gomiko'),
-        ),
-        body: Center(
-          child: _pages.elementAt(_selectedIndex),
-        ),
-        bottomNavigationBar: SizedBox(
-          height: 75,
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            selectedItemColor: bottomNavSelectedColor,
-            unselectedItemColor: bottomNavUnselectedColor,
-            currentIndex: _selectedIndex,
-            onTap: _onDestinationSelected,
-            showUnselectedLabels: true,
-            selectedLabelStyle: const TextStyle(fontSize: 12, height: 2.5),
-            unselectedLabelStyle: const TextStyle(fontSize: 12, height: 2.5),
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Iconsax.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Iconsax.search_favorite),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Iconsax.camera),
-                label: 'Scan',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Iconsax.medal),
-                label: 'Leaderboard',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Iconsax.user),
-                label: 'Profile',
-              ),
-            ],
+      appBar: AppBar(
+        title: const Text('Gomiko'),
+      ),
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: bottomNavSelectedColor,
+        unselectedItemColor: bottomNavUnselectedColor,
+        currentIndex: _selectedIndex,
+        onTap: _onDestinationSelected,
+        showUnselectedLabels: true,
+        selectedLabelStyle: const TextStyle(fontSize: 12, height: 2.5),
+        unselectedLabelStyle: const TextStyle(fontSize: 12, height: 2.5),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.home),
+            label: 'Home',
           ),
-        ));
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.search_favorite),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.camera),
+            label: 'Scan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.medal),
+            label: 'Leaderboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Iconsax.user),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
