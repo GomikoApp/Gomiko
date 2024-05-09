fed2120120ffccee3da39bb7d97a4d2f4cc51985
SLuo5991 <sluo5991@gmail.com>
2024-05-09 12:27:13 -0400

    Styled the profile section
    - Added a placeholder icon for the user profile image
    - Added placeholder name and email
    - Added icon to go to Edit profile page
    - Created edit profile page under view/profile

--- lib/views/profile_screen.dart
+++ lib/views/profile_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:iconsax/iconsax.dart';
+
+// Auth
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+// Views
+import 'package:recycle/views/profile/edit_profile.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key, required this.title}) : super(key: key);
@@ -27,18 +33,71 @@ class ProfilePageState extends State<ProfilePage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Padding(
-          padding: const EdgeInsets.only(left: 16.0, top: 16.0),
-          child: Text(widget.title,
-              style:
-                  const TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-        ),
+        title: Text(widget.title,
+            style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
       ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text("Hello, world! I'm ${widget.title}."),
-          ElevatedButton(onPressed: signOut, child: const Text("Sign Out")),
-        ]),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          // User Profile Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  child: const CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Color(0xff98cb51),
+                    child: Icon(
+                      Iconsax.user,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("John Doe",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Text(
+                    "johndoe@gmail.com",
+                    style:
+                        TextStyle(fontSize: 12, fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.4,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            const EditProfile(title: 'Edit Profile'),
+                      ),
+                    );
+                  },
+                  child: const Icon(
+                    Iconsax.edit,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
