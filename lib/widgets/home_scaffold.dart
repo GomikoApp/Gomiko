58b2c56d5359b4e1fad46b72c40112d75bcd3084
SLuo5991 <sluo5991@gmail.com>
2024-05-16 17:00:50 -0400

    remove app bar

--- lib/widgets/home_scaffold.dart
+++ lib/widgets/home_scaffold.dart
@@ -44,9 +44,6 @@ class _MyHomeScaffoldState extends State<HomeScaffold> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gomiko'),
-      ),
       body: IndexedStack(
         index: _selectedIndex,
         children: _pages,
