49ef51cabad5bedf8083572beafc89a674de6d8b
SLuo5991 <sluo5991@gmail.com>
2024-09-12 18:20:03 -0400

    update delete account to delete posts, images, and user
    - when user deletes their account, it delete all posts, images and from
    user table in firestore, firebase auth and firebase storage

--- lib/views/profile/widgets/build_delete_account.dart
+++ lib/views/profile/widgets/build_delete_account.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:recycle/constants.dart';
@@ -17,26 +18,59 @@ class BuildDeleteAccount extends StatelessWidget {
 
   Future<void> deleteAccount() async {
     try {
-      // delete user data from firestore
-      await FirebaseFirestore.instance
-          .collection('users')
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .delete();
-      await FirebaseAuth.instance.currentUser!.delete();
-    } on FirebaseAuthException catch (e) {
-      if (kDebugMode) {
-        print('Failed to delete account: $e');
+      // delete all posts by user
+      String uid = FirebaseAuth.instance.currentUser!.uid;
+
+      QuerySnapshot postsSnapshot = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid', isEqualTo: uid)
+          .get();
+
+      // for each post, delete the post and the corresponding images in firebase storage
+      for (QueryDocumentSnapshot post in postsSnapshot.docs) {
+        // if the posts has images, delete them
+        String? imageUrl = post['image'];
+        if (imageUrl != null && imageUrl.isNotEmpty) {
+          try {
+            Reference imageRef = FirebaseStorage.instance.refFromURL(imageUrl);
+            await imageRef.delete();
+          } catch (e) {
+            if (kDebugMode) {
+              print('Failed to delete image: $e');
+            }
+          }
+        }
+
+        // delete the post from firestore
+        await post.reference.delete();
       }
 
+      // delete user data from firestore user collection
+      await FirebaseFirestore.instance.collection('users').doc(uid).delete();
+
+      // delete user from firebase auth
+      await FirebaseAuth.instance.currentUser!.delete();
+
+      if (kDebugMode) {
+        print("Account deleted successfully");
+      }
+    } on FirebaseAuthException catch (e) {
+      if (kDebugMode) {
+        print("Failed to delete account: $e");
+      }
+
+      // show error message
       if (e.code == 'requires-recent-login') {
+        // reauthenticate and delete account
         await _reauthenticateAndDelete();
       } else {
-        // show error message
+        // handle other errors
       }
     } catch (e) {
       if (kDebugMode) {
-        print('Failed to delete account: $e');
+        print("Failed to delete account: $e");
       }
+      // show error message
     }
   }
 
